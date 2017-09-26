6d3189172c4464561996f25e9aa7bcc61c56f221
Mavalfer <mvalfer92@gmail.com>
2017-09-26 10:22:46 +0200

    aurora form

---
+++ Desarrollo Cliente/ejercicioForm/1.js
@@ -0,0 +1,27 @@
+
+'use strict'; //usa un motor que analiza el codigo en mas detalle
+
+(function() {
+    
+    var formulario = document.getElementById("formulario");
+    formulario.addEventListener('submit', validarFormulario);
+    
+    function comprobarPreguntas() {
+        var preguntas = document.getElementsByName("radio");
+        for (i = 0; i < preguntas.length; i++) {
+            if (preguntas[i].checked) {
+                return true;
+            }
+            return false;
+        }
+    }
+
+    function validarFormulario(evento) {
+        if (comprobarPreguntas() == true) {
+            window.alert("El formulario se va a enviar");
+        } else {
+            evento.preventDefault();
+            window.alert("El formulario no se ha enviado");
+        }
+    }
+ }());
