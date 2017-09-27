643d3abf7e3057bcfd0fff9069bca2693d491c4e
Mavalfer <mvalfer92@gmail.com>
2017-09-27 12:42:20 +0200

    form terminado

--- Desarrollo Servidor/ejercicioForm/Request.php
+++ Desarrollo Servidor/ejercicioForm/Request.php
@@ -1,36 +1,57 @@
 <?php
-class Request { //ojo! el archivo de llama como la clase
-    
+class Request { //ojo! el archivo de llama como la clase, esto siempre es asi
+    /*
+    Diferencias entre java y php:
+    this.nombre seria $this->nombre       no static
+    Alumno.centro seria self::$centro     static
+    */
     /*
     Este metodo va a tratar de leer un parametro, si es que ha llegado.
     */
-    function get($nombreDelParametro) {
+    static function get($nombreDelParametro) {
         if (isset ($_GET[$nombreDelParametro])) {
             return $_GET[$nombreDelParametro];
         }
         return null;
     }
     
-    function post($nombreDelParametro) {
+    static function getpost($nombreDelParametro){
+        $valor = self::get($nombreDelParametro);
+        if ($valor == null) {
+            $valor = self::post($nombreDelParametro);
+        }
+        return $valor;
+    }
+    
+    static function post($nombreDelParametro) {
         if (isset ($_POST[$nombreDelParametro])) {
             return $_POST[$nombreDelParametro];
         }
         return null;
     }
     
-    function read($nombreDelParametro) {
+    static function postget(){
+        $valor = self::post($nombreDelParametro);
+        if ($valor == null) {
+            $valor = self::get($nombreDelParametro);
+        }
+        return $valor;
+    }
+    
+    static function read($nombreDelParametro) {
         /*if (isset ($_GET[$nombreDelParametro])) {
             return $_GET[$nombreDelParametro];
         } else if (isset ($_POST[$nombreDelParametro])) {
             return $_POST[$nombreDelParametro];
         }  esto es volver a escribir codigo, mejor: */
         
-        $valor = get($nombreDelParametro);
+        /*$valor = get($nombreDelParametro);
         
         if ($valor == null) {
             $valor = post($nombreDelParametro);
         }
-        return $valor;
-    }
-}
-?>
+        return $valor;*/
+        return self::postget($nombreDelParametro);  //alias
+        
+    } 
+} //la recomendacion es que los archivos php puros no se cierran con la interrogacion>
