8f8790d0c883428395ab82a1f3f3098d0b55704c
miguel <mvalfer92@gmail.com>
2017-10-02 19:30:57 +0200

    clienteform

--- DesarrolloCliente/formulario3/apsfiltroInput.js
+++ DesarrolloCliente/formulario3/apsfiltroInput.js
@@ -3,17 +3,47 @@
 (function() {
     
     var campoNombre = document.getElementById('nombre');
-    var campoApellido = document.getElementById('apellido');
+    var codigoPostal = document.getElementById('codPostal');
     
     campoNombre.addEventListener('change', validarNombre);
-    campoApellido.addEventListener('change', validarNombre);
+    codigoPostal.addEventListener('keyup', validarPostal);
     
     function validarNombre(evento) {
         var nombre = evento.target;
-        if (nombre.value.length > 3) {
-            document.getElementById('ayudaNombre').innerHTML = 
+        if (nombre.value.length < 3) {
+            document.getElementById('ayudaNombre').innerHTML = "Es muy corto";
+        } else {
+            document.getElementById('ayudaNombre').innerHTML = "*";
         }
     }
-    
+    /*
+    function validarPostal(e) {
+        var postal = e.target;
+        for (var i = 0; i < postal.value.length; i++) {
+            if (postal.getAttribute("caracteresPermitidos").includes(postal.value.charAt(i))) {
+                document.getElementById('ayudaPostal').innerHTML = "";
+            } else {
+                document.getElementById('ayudaPostal').innerHTML = "Caracter no valido";
+            }
+        }
+    }
+    */
+    function validarPostal(e) {
+        var postal = e.target;
+        if (postal.getAttribute("caracteresPermitidos").includes(postal.value)) {
+            document.getElementById('ayudaPostal').innerHTML = "";
+        } else {
+            document.getElementById('ayudaPostal').innerHTML = "Caracter no valido";
+        }
+    }
+
+    function validarMail(e) {
+        var mail = e.target;
+        if (mail.value.includes('@') == false || mail.value.charAt(0) == '@') {
+            document.getElementById('ayudaMail').innerHTML = "Falta @";
+        } else if (mail.value.charAt(0) == '@') {
+            document.getElementById('ayudaMail').innerHTML = "Direccion no valida ";
+        }
+    }
     
  }())
