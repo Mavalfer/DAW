f66d72bd27e9e6ea906addde067b20066068ce99
Mavalfer <mig>
2017-10-23 14:29:34 +0200

    pdo

---
+++ DesarrolloServidor/tema3/aacesoaPelo.php
@@ -0,0 +1,68 @@
+<?php
+require './classes/Autoload.php';
+//establezco la conexion (abrir, trabajar, cerrar)
+
+$servidor = 'localhost:3306';
+$baseDatos = 'dwes';
+$usuario = 'udwes';
+$clave = 'cdwes';
+try{
+    $conexion = new PDO(
+        'mysql:host=' . $servidor . ';dbname=' . $baseDatos,
+        $usuario,
+        $clave,
+        array(
+            PDO::ATTR_PERSISTENT => true, //atributo de persistencia, crea un pool de conexiones, es una forma agil de distribuir las conexiones
+            PDO::MYSQL_ATTR_INIT_COMMAND => 'set names utf8'
+        )
+    );
+}catch (PDOException $e){
+    //echo $e->errorInfo();
+    $conexion = null; //deja libre 
+}
+
+if ($conexion === null) {
+    //echo 'No se ha podido conectar';
+    header('Location: error.html'); //como antes hemos hecho un echo y ya se han mandado datos al cliente, ya no se puede poner el header, estoen un servidor en condiciones falla, esta prohibido mandar la cabecera despues de la respuesta
+    
+    exit(); //Esto lo para todo y aqui acaba, de ahi palante nada
+}
+?>
+<!doctype html>
+<html>
+<head></head>
+<body>
+    <h1>Conectado</h1>
+    <?php
+    //esto es solo de ejemplo, consultas sin preparar no
+    //tres formas de hacer insert con distintos parametros
+//    $sql = 'insert into car(id, marca, modelo) values (null, "ford", "fiesta")';
+//    $res = $conexion->query( $sql );
+//    echo Util::varDump($res);
+//    
+//    $sql = 'insert into car(marca, modelo) values ("mini", "cooper")';
+//    $res = $conexion->query( $sql );
+//    echo Util::varDump($res);
+//    
+//    $sql = 'insert into car values (null, "vw", "polo")';
+//    $res = $conexion->query( $sql );
+//    echo Util::varDump($res);
+    
+    $sql = 'insert into car values (null, "citroën", "c3")';
+    $res = $conexion->query( $sql );
+    if ($res !== false){
+        $insertados = $res->rowCount();    
+        echo 'Se han insertado ' . $insertados . '<br>';
+        $id = $conexion->lastInsertId(); //da el id del ultimo elemento insertado
+        echo 'Se han insertado con el id ' . $id . '<br>';
+    }   
+    echo Util::varDump($res);
+    ?>
+</body>
+</html>
+
+
+<?php
+/*cerrar la conexion*/
+$conexion = null;
+?>
---
+++ DesarrolloServidor/tema3/scripts/scripts.sql
@@ -0,0 +1,30 @@
+/*1º crear la base de datos*/
+create database dwes
+default character set utf8
+collate utf8_unicode_ci;
+
+/*2º crear el usuario administrador de la base de datos*/
+create user udwes@localhost
+identified by 'cdwes';
+
+grant all
+on dwes.* to
+udwes@localhost;
+
+flush privileges;
+
+/*3º crear las tablas*/
+
+create table if not exists car (
+    id bigint not null auto_increment,
+    marca varchar(30) not null,
+    modelo varchar(40) not null,
+    primary key (id),
+    unique(marca, modelo)
+) engine=innodb default character set = utf8 collate utf8_unicode_ci;
+
+create table if not exists carold (
+    marca varchar(30) not null,
+    modelo varchar(40) not null,
+    primary key (marca, modelo)
+) engine=innodb default character set = utf8 collate utf8_unicode_ci;
