23445326ce75cf1746297b160b15d567afa75c16
Miguel <mvalfer92@gmail.com>
2017-11-21 19:32:09 +0000

    asd

---
+++ DesarrolloServidor/agenda/usuario/doactivar.php
@@ -0,0 +1,24 @@
+<?php
+require '../classes/AutoLoad.php';
+
+$db = new DataBase();
+$gestor = new ManageUsuario($db);
+
+$id = Request::read('id');
+$mail = Request::read('mail');
+
+$user = $gestor->get($id);
+
+if ($user !== null) {
+    if ($user->getCorreo() === $mail) {
+        $user->setVerificado(1);
+        $gestor->edit($user);
+        header('Location: ../index.php?ver=true');
+    } else {
+        header('Location: https://www.google.es/');
+    }
+} else {
+    header('Location: https://www.google.es/');
+}
+
+$db->closeConnection();
