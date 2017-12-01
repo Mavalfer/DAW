c44d772922da0141182effa7aa7f1cac63e4cf64
Miguel <mvalfer92@gmail.com>
2017-12-01 17:56:26 +0000

    asd

--- DesarrolloCliente/jQuery/ej4formularioJqueryHTML/script.js
+++ DesarrolloCliente/jQuery/ej4formularioJqueryHTML/script.js
@@ -1,14 +1,41 @@
+/*global $*/
 $(function() {
     
     // function validarForm(e, ) { /*Event, functions array*/
         
     // }
     
-    function validarTextField(valor, clave, error = "Campo incorrecto") {
-        if (clave.test(valor)) {
+    function validarTextField(input, clave = /^\w/, error = "Campo incorrecto") {
+        var r = false;
+        
+        if (clave.test(input.text())) {
             error = "";
+            r = true;
         }
-        return error;
+        if (error !== "") {
+            var span = $('<span>' + error + '</span>');
+            input.after(span);
+        }
+        return r;
     }
     
+    $("#formInscripcion").submit(function(e) {
+        var mail = validarTextField($('#email'), /^(([^<>()\[\]\.,;:\s@\"]+(\.[^<>()\[\]\.,;:\s@\"]+)*)|(\".+\"))@(([^<>()[\]\.,;:\s@\"]+\.)+[^<>()[\]\.,;:\s@\"]{2,})$/i);
+        var pass = false;
+        var pass1 = $('#password').text();
+        var pass2 = $('#verificar').text();
+        if (pass1 === pass2) {
+            pass = validarTextField(pass1);
+        }
+        var nombre = validarTextField($('#nombre'));
+        var apellidos = validarTextField($('#apellidos'));
+        var provincia = validarTextField($('#provincia'), /^\w{2}$/);
+        var postal = validarTextField($('#codPostal'), /^\d{5}$/);
+        var telefono = validarTextField($('#telefono'), /^(\d{3}-){2}\d{3}$/);
+        var fecha = validarTextField($('#fecha'));
+        
+        if (!mail || !pass || !nombre || !apellidos || !provincia || !postal || !telefono || !fecha) {
+            e.preventDefault();
+        }
+    });
 });
